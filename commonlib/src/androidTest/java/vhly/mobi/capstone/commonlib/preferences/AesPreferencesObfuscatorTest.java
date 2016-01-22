8acddc65128a33797c52609642249de877dc145f
Zhang Xiaohe <vhly@163.com>
2016-01-22 16:07:16 +0800

    test: Add ProtectedPreferences Test

---
+++ commonlib/src/androidTest/java/vhly/mobi/capstone/commonlib/preferences/AesPreferencesObfuscatorTest.java
@@ -0,0 +1,46 @@
+package vhly.mobi.capstone.commonlib.preferences;
+
+import android.test.AndroidTestCase;
+import mobi.vhly.capstone.commonlib.preferences.AesPreferencesObfuscator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vhly[FR]
+ * Date: 16/1/22
+ * Email: vhly@163.com
+ */
+public class AesPreferencesObfuscatorTest extends AndroidTestCase {
+
+    private byte[] mPassword;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        mPassword = new byte[]{
+                0x22, 0x43, (byte) 0x87, (byte) 0xFF,
+                0x22, 0x43, (byte) 0x87, (byte) 0xFF,
+                0x22, 0x43, (byte) 0x87, (byte) 0xFF,
+                0x22, 0x43, (byte) 0x87, (byte) 0xFF
+        };
+
+    }
+
+    public void testObfuscateString() {
+        AesPreferencesObfuscator obfuscator = new AesPreferencesObfuscator(mContext, mPassword);
+
+        String origString = "Hello World";
+
+        String string = obfuscator.obfuscateString(origString);
+
+        assertNotNull(string);
+
+        String s = obfuscator.unobfuscateString(string);
+
+        assertNotNull(s);
+
+        assertEquals(origString, s);
+
+    }
+
+}
---
+++ commonlib/src/androidTest/java/vhly/mobi/capstone/commonlib/preferences/ProtectedPreferencesTest.java
@@ -0,0 +1,76 @@
+package vhly.mobi.capstone.commonlib.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.test.AndroidTestCase;
+import mobi.vhly.capstone.commonlib.preferences.AesPreferencesObfuscator;
+import mobi.vhly.capstone.commonlib.preferences.ProtectedPreferences;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vhly[FR]
+ * Date: 16/1/22
+ * Email: vhly@163.com
+ */
+public class ProtectedPreferencesTest extends AndroidTestCase {
+
+    private SharedPreferences mSharedPreferences;
+    private byte[] mPassword;
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mSharedPreferences = mContext.getSharedPreferences("test", Context.MODE_PRIVATE);
+        mPassword = "AAAAAAAAAAAAAAAA".getBytes();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        mSharedPreferences = null;
+        mPassword = null;
+        super.tearDown();
+    }
+
+    public void testPutString(){
+
+        Context context = getContext();
+
+        AesPreferencesObfuscator obfuscator = new AesPreferencesObfuscator(context, mPassword);
+
+        ProtectedPreferences preferences = new ProtectedPreferences(mSharedPreferences, obfuscator);
+
+        String origValue = "I'm a access token";
+
+        String key = "access.token";
+
+        preferences.putString(key, origValue);
+
+        String token = preferences.getString(key, null);
+
+        assertNotNull(token);
+
+        assertEquals(origValue, token);
+
+    }
+
+    public void testPutInt(){
+        Context context = getContext();
+
+        AesPreferencesObfuscator obfuscator = new AesPreferencesObfuscator(context, mPassword);
+
+        ProtectedPreferences preferences = new ProtectedPreferences(mSharedPreferences, obfuscator);
+
+        int origValue = 998;
+
+        String key = "access.port";
+
+        preferences.putInt(key, origValue);
+
+        int port = preferences.getInt(key, -1);
+
+        assertEquals(origValue, port);
+
+    }
+
+}
